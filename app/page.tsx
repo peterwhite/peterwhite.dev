e4fb63c323492c50b2e7ebbb883c29694240c6b7
Peter White <peterwhite@users.noreply.github.com>
2022-12-10 19:41:59 +0100

    wip

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function Page() {
+  return (
+    <div className="space-y-8">
+      <h1 className="text-xl font-medium text-gray-300">Examples</h1>
+
+      <div className="space-y-10 text-white">Nothing to see here</div>
+    </div>
+  );
+}
---
+++ app/test/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="space-y-8">
+      <h1 className="text-xl font-medium text-gray-300">Peter Example</h1>
+    </div>
+  );
+}
