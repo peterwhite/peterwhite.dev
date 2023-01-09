60247d9a8eb9fcb67fd653a2daa597ef17b1e384
Peter White <peterwhite@users.noreply.github.com>
2023-01-09 09:00:00 +0100

    Add a newsletter page

---
+++ app/newsletter/page.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/no-unescaped-entities */
+export default function Page() {
+  return (
+    <div className="space-y-12">
+      <section id="about">
+        <h3 className="mb-4 text-xl font-medium">Newsletter</h3>
+        <div className="prose prose-zinc dark:prose-invert">
+          <p>
+            Thanks for your interest in my newsletter! Coming soon - email me at
+            newsletter at this domain to join now.
+          </p>
+        </div>
+        <div className="prose prose-zinc dark:prose-invert prose-headings:dark:text-zinc-200 "></div>
+      </section>
+    </div>
+  );
+}
