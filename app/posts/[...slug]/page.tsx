5f8442a62f93ad7cca64d9350210ef12d6ca518a
Peter White <peterwhite@users.noreply.github.com>
2023-01-15 16:32:31 +0100

    Add page titles

--- app/posts/[...slug]/page.tsx
+++ app/posts/[...slug]/page.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
 
-interface BlogPostProps {
+export interface BlogPostProps {
   params: {
     slug: string[];
   };
