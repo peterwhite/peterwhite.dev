61f4c07793bd1a7d2a06f5635cc449bb2a487438
Peter White <peterwhite@users.noreply.github.com>
2023-01-21 12:48:18 +0100

    Add homepage meta

--- app/head.tsx
+++ app/head.tsx
@@ -5,6 +5,10 @@ export default function Head() {
     <>
       <title>Peter White</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta
+        name="description"
+        content="Full Stack Developer in Norway, happily building things on the internet since 2008"
+      />
       <Script src="/theme.js" />
     </>
   );
