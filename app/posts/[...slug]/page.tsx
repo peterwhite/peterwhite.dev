19bf80b546cb548338f9724f6271575bfc2e5caa
Peter White <peterwhite@users.noreply.github.com>
2023-03-29 09:45:40 +0200

    Add generateStaticParams for static generation

--- app/posts/[...slug]/page.tsx
+++ app/posts/[...slug]/page.tsx
@@ -12,6 +12,9 @@ export interface BlogPostProps {
   };
 }
 
+export const generateStaticParams = async () =>
+  allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
+
 export async function generateMetadata({
   params,
 }: {
