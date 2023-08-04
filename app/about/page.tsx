bfcc7a31f772057e3c54d9b91f52d2e1b9f288a5
Peter White <peterwhite@users.noreply.github.com>
2023-08-04 16:49:57 +0200

    Update about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -30,11 +30,11 @@ export default function Page() {
             </ul>
             <h2>Bio</h2>
             <h3>Job Title</h3>
-            <p>Peter White, Tech Lead at IndyRiot</p>
+            <p>Peter White, Senior Software Engineer at Graphy</p>
             <h3>Long, 3rd Person</h3>
             <p>
-              Peter White is a Tech Lead at IndyRiot, where he
-              builds software for making better online communities. He is a
+              Peter White is a Senior Software Engineer at Graphy, where he
+              builds software that gives everyone data superpowers. He is a
               product engineer and full-stack developer with over a decade of
               experience of building startups and internet-based products, in
               both founding and contributing roles. He is also a mentor, advisor
@@ -43,8 +43,8 @@ export default function Page() {
             <h3>Long, 1st Person</h3>
             <p>
               Hey, I'm Peter! Originally from Yorkshire in the UK, I migrated to
-              Norway in 2021. I am a Tech Lead at IndyRiot, where
-              I build software for making better online communities. I am a
+              Norway in 2021. I am a Senior Software Engineer at Graphy, where I
+              build software that gives everyone data superpowers. I am a
               product engineer and full-stack developer with over a decade of
               experience building startups and internet-based products, in both
               founding and contributing roles. I am also a mentor, advisor, and
@@ -52,13 +52,13 @@ export default function Page() {
             </p>
             <h3>Short, 3rd Person</h3>
             <p>
-              Peter White is a Tech Lead at IndyRiot, where he
-              builds software for making better online communities
+              Peter White is a Senior Software Engineer at Graphy, where he
+              builds software that gives everyone data superpowers.
             </p>
             <h3>Short, 1st Person</h3>
             <p>
-              Hey, I'm Peter! I am a Tech Lead at IndyRiot, where
-              I build software for making better online communities
+              Hey, I'm Peter! I am a Senior Software Engineer at Graphy, where I
+              build software that gives everyone data superpowers.
             </p>
           </div>
         </div>
