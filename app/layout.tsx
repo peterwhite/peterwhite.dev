a0b2a9b65f49b056c6e5f750afe7e8a74e4a12a8
Peter White <peterwhite@users.noreply.github.com>
2024-09-12 14:49:46 +0200

    Add Vector

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,13 @@ export default function RootLayout({
       className={`${inter.variable} ${jetBrains.variable} invisible antialiased
     `}
     >
+      <Script id="vector-script" strategy="afterInteractive">
+        {`
+          !function(e,r){try{if(e.vector)return void console.log("Vector snippet included more than once.");var t={};t.q=t.q||[];for(var o=["load","identify","on"],n=function(e){return function(){var r=Array.prototype.slice.call(arguments);t.q.push([e,r])}},c=0;c<o.length;c++){var a=o[c];t[a]=n(a)}if(e.vector=t,!t.loaded){var i=r.createElement("script");i.type="text/javascript",i.async=!0,i.src="https://cdn.vector.co/pixel.js";var l=r.getElementsByTagName("script")[0];l.parentNode.insertBefore(i,l),t.loaded=!0}}catch(e){console.error("Error loading Vector:",e)}}(window,document);
+          vector.load("fec0c915-ad42-49ea-8110-6143258443c3");
+        `}
+      </Script>
+
       <body className="relative bg-zinc-100 text-zinc-800 selection:bg-vercel-blue/90 selection:text-white dark:bg-zinc-900 dark:text-zinc-200">
         <Script src="/theme.js" />
         <AnalyticsWrapper />
@@ -42,7 +49,7 @@ export default function RootLayout({
 
         <GlobalNavigation />
 
-        <div className="mx-auto max-w-4xl space-y-8 py-8 px-4 sm:my-4 ">
+        <div className="mx-auto max-w-4xl space-y-8 px-4 py-8 sm:my-4 ">
           <div className="space-y-4 py-6">
             <BreadCrumbs />
 
